91930e7cff52a559ca820ef28dd4fe28a475b367
Scott J Maddox <scottjmaddox@gmail.com>
2018-10-10 11:11:47 -0500

    add doc-comment usage example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
 //! A logger for the wasm32-unknown-unknown target that prints all messages to
 //! the web console
+//!
+//! ## Usage
+//!
+//! ```no_run
+//! #[macro_use]
+//! extern crate log;
+//! extern crate web_console_logger;
+//! extern crate wasm_bindgen;
+//!
+//! use wasm_bindgen::prelude::*;
+//!
+//! #[wasm_bindgen]
+//! pub fn main() {
+//!     web_console_logger::init().unwrap();
+//!     // OR
+//!     // web_console_logger::init_with_level(log::Level::Warn).unwrap();
+//!
+//!     warn!("This example message will be printed to the web browser's javascript console.");
+//! }
+//! ```
+//!
+//! ## License
+//!
+//! Web Console Logger is licensed under either of
+//!
+//!  * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+//!    http://www.apache.org/licenses/LICENSE-2.0)
+//!  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+//!    http://opensource.org/licenses/MIT)
+//!
+//! at your option.
 
 extern crate log;
 extern crate wasm_bindgen;
